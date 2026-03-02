9f9c2915896b904f723a63a171ba43ce262fe33a
Chun-Wei Su <chun-weisu@Chun-WeideMacBook-Air.local>
2026-03-02 21:03:45 +0800

    Add PWA icons and env variables

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9f4fdttcggk"
+    "revision": "0.pkdrq2v19u"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
